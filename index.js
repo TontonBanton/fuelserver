bc6657e945c28479143540b56a2aa10e065226ca
TontonBanton <pancla13ulas@gmail.com>
2024-06-24 15:44:26 +0800

    Basic test

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const express = require('express')
 const app = express()
 
+app.get('/', (req, res)=> {
+  res.json("heheheh")
+})
+
 app.listen(3001, ()=> {
-  console.log("OK")
+  console.log(`Server running at port 3001`)
 })
